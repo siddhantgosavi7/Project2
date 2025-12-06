4300a78f2749225f2b6100f0170f6e17a15e5936
atharvWaste <atharvawaste16@gmail.com>
2025-12-06 18:17:45 +0530

    starting for the form logic

--- frontend/jsFILES/index.js
+++ frontend/jsFILES/index.js
@@ -1,17 +1,18 @@
 let array = [
-    "Humidity is the measure of water vapor in the air", 
-    "Wind: This indicates both the speed and direction of the wind",
-"Atmospheric pressure: The pressure exerted by the weight of the atmosphere",
-`Cloud cover: Describes the amount of the sky that is covered by clouds, with terms like "clear," "partly cloudy," or "overcast".`,
-`Visibility: The distance one can see clearly. It can be affected by fog, rain, or snow. `];
+  "Humidity is the measure of water vapor in the air",
+  "Wind: This indicates both the speed and direction of the wind",
+  "Atmospheric pressure: The pressure exerted by the weight of the atmosphere",
+  `Cloud cover: Describes the amount of the sky that is covered by clouds, with terms like "clear," "partly cloudy," or "overcast".`,
+  `Visibility: The distance one can see clearly. It can be affected by fog, rain, or snow. `,
+];
 let i = 0;
 
 function printTerms() {
-    setInterval(() => {
-        document.getElementById("Jshelp").value = array[i];
-        i = (i + 1) % array.length; // 6%5=1 
-        console.log("op");
-    }, 3000);
+  setInterval(() => {
+    document.getElementById("Jshelp").value = array[i];
+    i = (i + 1) % array.length; // 6%5=1
+    console.log("op");
+  }, 3000);
 }
 
 // it will feel like an infinite loop pan tasa kani nahi honr  it will not block rest of code.
